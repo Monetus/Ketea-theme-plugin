22e851161d8147fb0e2b7a74d5d686f5f04aa94d
monetus <UFOsFollowMe@gmail.com>
2016-12-02 11:26:49 -0600

    removed debug statements, and added a proc for selectively deleting bindings

--- hotkey-plugin.tcl
+++ hotkey-plugin.tcl
@@ -34,12 +34,10 @@ namespace eval hotkeys:: {
     # type into box
     set text_length [string length $text]
     for {set index 0} {$index < $text_length} {incr index} {
-        set letter [string index $text $index]
-        scan $letter %c keynum
-        pdsend "$mytoplevel key 1 $keynum 0"
-        pdwindow::post $letter
+      set letter [string index $text $index]
+      scan $letter %c keynum
+      pdsend "$mytoplevel key 1 $keynum 0"
     }
-    pdwindow::post "\nobj typed\n"
   }
 
   # looking around pd's source code, you'll find useful variables, like the array \
@@ -54,14 +52,13 @@ namespace eval hotkeys:: {
     if {[winfo class $tkcanvas] eq "Canvas"} {
       # find the window holding the canvas.
       set mytoplevel [winfo parent $tkcanvas]
+      # check that edit mode is on to be sure that you aren't typing into a symbol obj.
       if {!$::editingtext($mytoplevel) && $::editmode($mytoplevel)} {
         # use this to create the object on the canvas window
         menu_send_float $mytoplevel obj 0
-        pdwindow::post "$mytoplevel - created obj\n"
         type_into_obj $mytoplevel "metro 100"
       }
     }
-    pdwindow::post "$mytoplevel - proc fired\n"
   }
   # use the plus sign to keep from erasing other bindings to this key, unless that is what you want
   bind all <Key-n> {+hotkeys::create_metro %W}
@@ -95,6 +92,39 @@ append overwritten_body {
 
 proc pdtk_text_editing "$overwritten_args" "$overwritten_body"
 
-# clean up these globals, as they are only needed above.
+# clean up these globals, as they are only needed in the proc above.
 unset overwritten_args
 unset overwritten_body
+
+
+# Now, there are two problems with the code I wrote above.
+# 1. The state of the editingtext() array is not always correct. It doesn't happen\
+    often at all, but you can get the value backwards by confusing the gui.       \
+    Thankfully, it isn't much of a bug, as doing much of anything to the gui will \
+    reset the value correctly.
+# 2. When editing text, the removal of the binding removes EVERY binding to n and \
+    only resets the metro one.  This isn't a problem for an unused key like n, but\
+    if you wanted to add, then remove, some behavior to an existing hotkey you'd  \
+    need to selectively delete bindings.  Introspection to the rescue again:
+
+proc delete_binding {bind_tag key_combination binding} {
+  # bind all <Key-n> returns a list of all the current bindings to key-n.
+  set all_bindings [bind $bind_tag <$key_combination>]
+  # find where the binding you want to delete starts in the list by searching for \
+    the first word in the binding.
+  set binding_index [lsearch -exact $all_bindings [lindex $binding 0]]
+  # if it exists
+  if {$binding_index ne -1} {
+    # then, for the last index, find the size of the binding - 1 + starting index
+    set all_bindings \
+      [lreplace $all_bindings $binding_index [expr $binding_index+[llength $binding]-1]]
+    # yet again, quotes rather than curly braces to force interpretation
+    bind $bind_tag <$key_combination> "$all_bindings"
+  }
+}
+
+#  Sadly this proc still suffers because of the way it searches and deletes the   \
+  binding from the list of all bindings.  Regular expressions aren't my strong suit.
+
+# I left this proc at the bottom, because you probably don't need it, but if you  \
+  do, just place it in the namespace above and use it where you'd like.
